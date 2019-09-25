b7494a66e6dad0099ce23566bacf9e2b79c0229f
Killassa <greg12876@gmail.com>
2019-09-25 16:00:30 +0200

    Add Excel classes

---
+++ Excel/ExcelFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace ExcelExport.Excel
+{
+    class ExcelFile
+    {
+
+        public List<ExcelSheet> excelSheets { get; private set; }
+
+        public ExcelFile(string filePath)
+        {
+            Application excelFile = new Application();
+            
+
+            foreach()
+            {
+                excelSheets.Add(new ExcelSheet());
+            }
+        }
+    }
+}
