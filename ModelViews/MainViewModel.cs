b2299d7f96478c74ca10a6012e59a4f9e64473a5
_Greenkiller_ <ericperonetti00@gmail.com>
2019-10-02 15:29:17 +0200

    Adding command for "open" buttons

--- ModelViews/MainViewModel.cs
+++ ModelViews/MainViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using ExcelExport.Utils;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ExcelExport.ModelViews
 {
@@ -13,6 +16,26 @@ namespace ExcelExport.ModelViews
         {
             get => _testText;
             set => SetProperty(ref _testText, value);
+        }
+
+        public ICommand OpenFiles
+        {
+            get => new Command(() => 
+            {
+                OpenFileDialog ofdFile = new OpenFileDialog();
+                ofdFile.Filter = "Excel files (*.xlsx)|*.xlsx";
+                ofdFile.Title = "Choose a file to open";
+                ofdFile.CheckFileExists = true;
+                ofdFile.CheckPathExists = true;
+                ofdFile.Multiselect = false;
+                ofdFile.InitialDirectory = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\Downloads");
+                if (ofdFile.ShowDialog() != true) return;
+
+                string fileName = ofdFile.FileNames?.First();
+                if (string.IsNullOrWhiteSpace(fileName)) return;
+
+
+            });
         }
     }
 }
